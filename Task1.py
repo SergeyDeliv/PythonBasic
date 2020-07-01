20ce5df583248f14facb472ab1a0872d1bb08d55
EgorkinSergey <deliv88@mail.ru>
2020-07-01 22:38:20 +0800

    Выполнил задание 1 урока

---
+++ Task1.py
@@ -0,0 +1,9 @@
+a = 55
+print(a)
+b = "Привет!"
+print(b)
+firstname = input("Введите своё имя ")
+lastname = input("Введите свою фамилию ")
+age = input("Введите свой возраст ")
+print("Здравствуй " + firstname + " " + lastname + ". Вам " + age)
+
